5e4aeda344cbf71d1ea8e085c9f226300debee3b
Alexander Tyllesen <alex143r@stud.kea.dk>
2021-11-12 16:16:06 +0100

    "test"

--- amazon/shop.php
+++ amazon/shop.php
@@ -2,9 +2,10 @@
 
 $data = json_decode(file_get_contents("shop.txt"));
 foreach ($data as $item) {
-    echo "<div>
+    echo "
+        <div>
             <div>{$item->id}</div>
             <div>{$item->title}</div>
             <img src='https://coderspage.com/2021-F-Web-Dev-Images/{$item->image}'>
-            </div>";
+        </div>";
 };
